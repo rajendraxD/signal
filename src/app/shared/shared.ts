d083c5018efc5bc54de05db7722ad0ff21a45b8f
rajendraxD <rajendraxd1@gmail.com>
2025-07-23 12:16:11 +0530

    done some changes

--- src/app/shared/shared.ts
+++ src/app/shared/shared.ts
@@ -2,6 +2,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { Header } from './reusableComponent/header/header';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 export const shardModule = [
   RouterModule, //for routing
@@ -10,4 +11,5 @@ export const shardModule = [
   RouterOutlet,
   CommonModule,
   Header,
+  MatIconModule
 ];
